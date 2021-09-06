0544ca321715fabd13bbaca3bbaffa000f5a0a41
Ruben <rubenmrosario87@gmail.com>
2021-09-05 21:58:27 -0400

    add index and about pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout';
+
+ const About = () => {
+    return (
+        <Layout pageTitle = "About">
+            <p> this is the about page.</p>
+        </Layout>
+    )
+}
+
+export default About;
