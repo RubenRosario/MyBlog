e53e2813c077e573b58c54c9f6b7d84d557d6640
Ruben <rubenmrosario87@gmail.com>
2021-09-07 13:37:48 -0400

    add gatsby-plugin-image and render an image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
         <p> The content will show up here.</p>
+        <StaticImage src = "../images/download.jpeg" alt="Ruben picture"/>
     </Layout>
   )
 }
