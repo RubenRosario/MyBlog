294f8a95dfa41633c572b6846f664fbcc0d0d6e8
Ruben <rubenmrosario87@gmail.com>
2021-09-09 21:32:05 -0400

    use graphql to pull data from the data layer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,15 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "MyBlog",
+    title: "My First Gatsby Blog",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-image", "gatsby-plugin-sharp", 
+  {
+    resolve: "gatsby-source-filesystem",
+    options:
+    {
+      name: "blog",
+      path: `${__dirname}/blog`
+    }
+  }],
 };
